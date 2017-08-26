e63b1c253dbae4c46124aa9980d6c350d5f1ed8e
John Beech <github@mkv25.net>
2017-08-26 14:50:12 +0100

    Lay out symbols for three symbol conversion

--- write-instructions.js
+++ write-instructions.js
@@ -12,15 +12,29 @@ var tileTemplate = `{
     "$ref": "/data/tiles/{name}.json"
   }
 }`
+var htmlTemplate = `{
+  "asset": "{name}.html",
+  "template": "/tile-renderer.html",
+  "renderer": {
+    "$ref": "/data/renderer/html-renderer.json"
+  },
+  "data": {
+    "$ref": "/data/tiles/{name}.json"
+  }
+}`
 
 var instructions = []
 find('./data/tiles/*.json')
   .then(files => files.map(filepath => path.basename(filepath)))
   .then(files => files.map(filename => filename.slice(0, -5)))
-  .then(files => files.map(createInstruction))
-  .then(instructions => write(outputFile, JSON.stringify(instructions, null, 2), 'utf8'))
+  .then(files => files.forEach((file) => {
+    createInstruction(file, tileTemplate)
+    createInstruction(file, htmlTemplate)
+  }))
+  .then(() => write(outputFile, JSON.stringify(instructions, null, 2), 'utf8'))
   .then(result => console.log(`Created ${outputFile}`))
 
-function createInstruction(name) {
-  return JSON.parse(tileTemplate.replace(/{name}/g, name))
+function createInstruction(name, template) {
+  let instruction = JSON.parse(template.replace(/{name}/g, name))
+  instructions.push(instruction)
 }
